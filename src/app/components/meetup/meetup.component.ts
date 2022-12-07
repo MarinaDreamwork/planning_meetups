9800dafae26071d120c7add47775b23f009941e9
Marina Trushina <silaevamo@gmail.com>
2022-12-07 09:28:58 +0300

    created frame for the Meetup entity

---
+++ src/app/components/meetup/meetup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup',
+  templateUrl: './meetup.component.html',
+  styleUrls: ['./meetup.component.scss']
+})
+export class MeetupComponent {
+
+}
