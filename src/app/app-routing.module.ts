4e79cd69ae54a02f5fa8efbe1606c38293f4fffb
Marina Trushina <silaevamo@gmail.com>
2022-12-15 21:07:33 +0300

    Added Angular material styling, added Intro Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AdminComponent } from './components/admin/admin.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { MeetupListComponent } from './components/meetup-list/meetup-list.component';
 import { AdminGuard } from './guards/admin.guard';
+import { IntroComponent } from './components/intro/intro.component';
 
 const routes: Routes = [
   { path: 'login', component: AuthComponent },
   { path: 'meetups', component: MeetupListComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] }
+  { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
+  { path: 'introduction', component: IntroComponent }
 ];
 
 @NgModule({
